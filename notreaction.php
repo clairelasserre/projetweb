900cb297ef72be8a61a2abbefbba2d5af3a811e3
clairelasserre <clairemarie.lasserre@gmail.com>
2016-12-03 17:47:32 +0100

    Add files via upload

---
+++ notreaction.php
@@ -0,0 +1,69 @@
+<html>
+ 
+   <head>
+      <meta charset="UTF-8"/>
+      <meta name="Claire" content="Nom de l'auteur"/>
+      <meta name="base du projet" content="Mots clefs relatifs à cette page"/>
+      <title>Page Permanente</title>
+       <!-- CSS Bootstrap -->
+        <link href="css/bootstrap.css" rel="stylesheet">
+ 
+        <!-- CSS Perso -->
+        <link href="css/styleheader-footer.css" rel="stylesheet">
+ 
+   </head>
+ 
+   <body>
+       <link rel="stylesheet" type="text/css" href="styleheader-footer.css" />
+      <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="js/jquery.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="js/bootstrap.js"></script>
+      
+       
+  <nav class="navbar navbar-light bg-faded">
+  <a class="navbar-brand" href="#">PAS DE GASPIX</a>
+  <ul class="nav navbar-nav">
+    <li class="nav-item active">
+      <a class="nav-link" href="#">Notre action <span class="sr-only">(current)</span></a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Les offres</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Faire un don</a>
+    </li>
+    <li class="dropdown">
+  <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Mon profil
+  <span class="caret"></span></button>
+  <ul class="dropdown-menu">
+    <li><a href="#">Voir mon profil </a></li>
+    <li><a href="#">Se déconnecter</a></li>
+  </ul>
+</li>  
+  </ul> 
+</nav>
+        
+        
+<br></br>
+<br></br>
+<h1>Le gaspillage en France</h1>
+<br></br>
+<h2>Le gaspillage en France gaspille 2000kg de blbaahudiuezbacuyp erhv rehvi rzjf mijzeoifjezf v    g cui hxjpoze hcgearuimocjrkabuhrhujruihfujerhabfjiocjebhrjfiojhbejifohbuijohbijojhbgvuiuhgvuijtyuiytyuiuytresdfghjkhgf</h2>
+<br></br>
+<br></br>
+<h1>Notre histoire</h1>
+<br></br>
+
+<h2>Le gaspillage en France gaspille 2000kg de blbaahudiuezbacuyp erhv rehvi rzjf mijzeoifjezf v    g cui hxjpoze hcgearuimocjrkabuhrhujruihfujerhabfjiocjebhrjfiojhbejifohbuijohbijojhbgvuiuhgvuijtyuiytyuiuytresdfghjkhgf</h2>
+<br></br>
+<br></br>
+<h1>Pas de Gaspix, comment ca marche ? </h1>
+<br></br>
+<h2>Le gaspillage en France gaspille 2000kg de blbaahudiuezbacuyp erhv rehvi rzjf mijzeoifjezf v    g cui hxjpoze hcgearuimocjrkabuhrhujruihfujerhabfjiocjebhrjfiojhbejifohbuijohbijojhbgvuiuhgvuijtyuiytyuiuytresdfghjkhgf</h2>
+<br></br>
+</font></div> 
+    <button type="button" class="btn btn-success"style="text-align: center"> Pret à offrir, en 1 clic propose un repas </button>
+</div>
+
+</html>
---
+++ paged'acceuil.php
@@ -0,0 +1,71 @@
+<html>
+ 
+   <head>
+      <meta charset="UTF-8"/>
+      <meta name="Claire" content="Nom de l'auteur"/>
+      <meta name="base du projet" content="Mots clefs relatifs à cette page"/>
+      <title>Page Permanente</title>
+       <!-- CSS Bootstrap -->
+        <link href="css/bootstrap.css" rel="stylesheet">
+ 
+        <!-- CSS Perso -->
+        <link href="css/styleheader-footer.css" rel="stylesheet">
+ 
+   </head>
+ 
+   <body>
+       <link rel="stylesheet" type="text/css" href="styleheader-footer.css" />
+      <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="js/jquery.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="js/bootstrap.js"></script>
+      
+       
+  <nav class="navbar navbar-light bg-faded">
+  <a class="navbar-brand" href="#">PAS DE GASPIX</a>
+  <ul class="nav navbar-nav">
+    <li class="nav-item active">
+      <a class="nav-link" href="#">Notre action <span class="sr-only">(current)</span></a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Les offres</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Faire un don</a>
+    </li>
+    <li class="dropdown">
+  <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Mon profil
+  <span class="caret"></span></button>
+  <ul class="dropdown-menu">
+    <li><a href="#">Voir mon profil </a></li>
+    <li><a href="#">Se déconnecter</a></li>
+  </ul>
+</li>
+
+
+
+    
+  </ul>
+  
+</nav>
+        
+<div style="border: #686868 5px solid; padding: 10px; -moz-border-radius: 15px; -webkit-border-radius: 15px"> 
+    <div style="text-align: center"><img  src="cuisine .jpg" width="500" height="500" /></div>
+    <h3>
+Jacquelines : "Je ne gaspille plus"
+</h3>
+    <button type="button" class="btn btn-success"style="text-align: center;font-size: 1.5em">Offrir un repas </button>
+</div>
+
+<div style="border: #686868 5px solid; padding: 10px; -moz-border-radius: 15px; -webkit-border-radius: 15px"> 
+    <div style="text-align: center"><img  src="Etre-en-pleine-forme-2.jpg" width="500" height="500" /></div>
+    <h3>
+Jean-Claude : "J'ai retrouvé le plaisir de bien diner"
+    </h3>
+     <button type="button" class="btn btn-success" style="text-align: center; font-size: 1.5em"> Consulter les offres près de chez moi  </button>
+</div>
+
+ 
+
+
+</html>
